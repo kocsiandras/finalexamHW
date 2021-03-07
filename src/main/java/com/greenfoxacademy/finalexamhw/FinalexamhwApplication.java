09312b9b033343aed3f66e8689d4857345615cbf
kocsiandras <kocsi.andras@yahoo.com>
2021-03-07 13:22:07 +0100

    gitignore added

---
+++ src/main/java/com/greenfoxacademy/finalexamhw/FinalexamhwApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.finalexamhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalexamhwApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FinalexamhwApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/greenfoxacademy/finalexamhw/FinalexamhwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.finalexamhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalexamhwApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
