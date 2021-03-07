9cdfff57cb15eef6980a6dfc808d98635bd50b5c
kocsiandras <kocsi.andras@yahoo.com>
2021-03-07 15:12:23 +0100

    set up for testing

---
+++ src/test/java/com/greenfoxacademy/finalexamhw/controllers/RegistrationControllerTest.java
@@ -0,0 +1,49 @@
+package com.greenfoxacademy.finalexamhw.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.greenfoxacademy.finalexamhw.models.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.hamcrest.core.Is.is;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class RegistrationControllerTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Autowired
+  ObjectMapper objectMapper;
+
+  @Test
+  public void noUsernameAdded() throws Exception {
+    User user = User.builder()
+        .username(null)
+        .password("pw")
+        .build();
+    mockMvc.perform(post("/register")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(user)))
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.message", is("Please give a username")));
+  }
+
+}
